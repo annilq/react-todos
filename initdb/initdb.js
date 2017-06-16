62203ae47215cfca93b98a27f6575c7f60da9d7c
annilq <410493517@qq.com>
2017-06-16 16:41:28 +0800

    优化model方法

--- initdb/initdb.js
+++ initdb/initdb.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var db = require("../config/db");
+mongoose.Promise = global.Promise;
 function initdb() {
   return mongoose.connect(db.url);
 }
--- routes/user/login.js
+++ routes/user/login.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bcrypt = require("bcrypt");
-var User = require("../../models/user");
+var User = require("../../lib/user");
 var Folder = require("../../lib/folder");
 
 // ROUTES FOR OUR API
@@ -11,26 +11,16 @@ router
   .route("/")
   // create a user (accessed at POST http://localhost:8080/api/users)
   .post(function(req, res) {
-    var user = new User(); // create a new instance of the Bear model
-    user.name = req.body.name;
-    user.password = req.body.password;
     // 生成10个字符的盐
-    User.findOne({ name: user.name }, function(err, data) {
-      if (err) res.send(err);
-      if (!data) {
-        res.json({ code: -1, message: "该用户不存在" });
-      } else {
-        bcrypt.hash(user.password, data.salt, function(err, hash) {
-          if (hash === data.password) {
-            req.session.uid = data._id;
-            // 登录成功后配置用户默认目录
-            Folder.initfolders(req.session.uid);
-            res.json({ code: 0, data: user, message: "登录成功" });
-          } else {
-            res.json({ code: -1, message: "密码错误" });
-          }
-        });
+    User.login(req.body.name, req.body.password).then(
+      function(uid) {
+        req.session.uid = uid;
+        Folder.initfolders(uid);
+        res.json({ code: 0, data: uid, message: "登录成功" });
+      },
+      function(msg) {
+        res.json({ code: -1, message: msg });
       }
-    });
+    );
   });
 module.exports = router;
